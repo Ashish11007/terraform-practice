5c7db7c41a2798f8b9b52f42f6b58e7af2a6dd58
unknown <ashishborate0070@gmail.com>
2026-07-23 20:40:12 +0530

    day-3

---
+++ day-3-vpc/var.tf
@@ -0,0 +1,40 @@
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    default = "10.0.0.0/24"
+}
+
+variable "public_az" {
+  default = "us-east-2a"
+}
+
+variable "private_subnet_cidr" {
+    default = "10.0.1.0/20"
+}
+
+variable "private_az" {
+    default = "us-east-2b"
+}
+
+variable "ssh_port" {
+   default = 22
+}
+
+variable "http_port" {
+    default = 80
+}
+
+variable "ami"{
+    default = "ami-078fe7ff43e10cf8c"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "key_name" {
+    default = "terraform"
+}
+
